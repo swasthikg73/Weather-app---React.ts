4c168b408bc1a7bdc396763b7a254e913d537f9b
swasthikg73 <swasthikg32@gmail.com>
2026-03-04 17:18:04 +0530

    Countries File added

--- src/API/Api.ts
+++ src/API/Api.ts
@@ -1,21 +1,15 @@
 import axios from "axios";
 import { weatherSchema } from "../schema/weatherSchema";
 
+import { GeoCodeSchema } from "@/schema/geoCodeSchema";
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 
 //Current day Weather
 export const getWeather = async ({ lat, lon }: { lat: number, lon: number }) => {
 
-
-    const res = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&APPID=1e83049831e7c8aab189fc9368340f7f`);
-    //https://api.openweathermap.org/data/3.0/onecall?lat=36&lon=86&exclude={part}&appid=c1c014a532a4196e5085c635ab75a36e
-    //one day
-    //const res = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=1e83049831e7c8aab189fc9368340f7f`)
-
-    //console.log(res.data);
-
-    console.log(res.data);
+    const res = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&APPID=${API_KEY}`);
 
     return weatherSchema.parse(res.data);  //This line validates data against the Zod schema (WeatherSchema).
 
@@ -24,10 +18,10 @@ export const getWeather = async ({ lat, lon }: { lat: number, lon: number }) => 
     // ❌ If data does NOT match → it throws an error
 }
 
-//5 day - hourly next
-// export const getWeatherForecast = async ({ lat, lon }: { lat: number, lon: number }) => {
 
-//     const res = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=1e83049831e7c8aab189fc9368340f7f`)
+//Geo Code
+export const getGeoCode = async (City: string) => {
+    const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${City}&limit=1&appid=${API_KEY}`);
+    return GeoCodeSchema.parse(res.data)
+}
 
-//     return ForecastSchema.parse(res.data)
-// }
--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,46 @@ import { AdditionalInfo } from "./components/cards/AdditionalInfo";
 import Map from "./components/Map";
 import { useState } from "react";
 import type { Cords } from "./Types/types";
+import { LocationDropdowns } from "./components/dropdowns/LocationDropdowns";
+import { useQuery } from "@tanstack/react-query";
+import { getGeoCode } from "./API/Api";
+
+import { MapTypesDropdowns } from "./components/dropdowns/MapTypesDropdowns";
 
 function App() {
-  const [cords, setCords] = useState<Cords>({ lat: 12.8855, lon: 74.8388 });
+  const [coordinates, setCords] = useState<Cords>({ lat: 0, lon: 0 });
+  const [location, setLocation] = useState("Delhi");
+  const [mapType, setMapType] = useState("clouds_new");
 
+  const { data } = useQuery({
+    queryKey: ["geocode", location],
+    queryFn: () => getGeoCode(location),
+  });
   const onMapClick = (lat: number, lon: number) => {
     setCords({ lat: lat, lon: lon });
+    setLocation("custom");
   };
 
+  const cords =
+    location === "custom"
+      ? coordinates
+      : { lat: data?.[0].lat ?? 28.61, lon: data?.[0].lon ?? 77.21 };
   return (
     <>
       <div className="flex flex-col gap-8 ">
-        <div className="md:grid grid-cols-2">
-          <CurrentWeather cords={cords} />
-          <Map cords={cords} onMapClick={onMapClick} />
-        </div>
+        <div className="flex gap-8 m-4">
+          <div className="flex gap-4">
+            <h1 className="text-2xl font-semibold">Location :</h1>
+            <LocationDropdowns location={location} setLocation={setLocation} />
+          </div>
 
+          <div className="flex gap-4">
+            <h1 className="text-2xl font-semibold">Map Type :</h1>
+            <MapTypesDropdowns mapType={mapType} setMapType={setMapType} />
+          </div>
+        </div>
+        <Map cords={cords} onMapClick={onMapClick} mapType={mapType} />
+        <CurrentWeather cords={cords} />
         <HourlyForecast cords={cords} />
         <DailyForecast cords={cords} />
         <AdditionalInfo cords={cords} />
